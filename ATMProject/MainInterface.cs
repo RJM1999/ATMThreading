273393b3a40720a406940080ab494853edf58b6f
Ross <Ross@DESKTOP-UT87NMM>
2019-03-21 13:51:42 +0000

    Pile of shite

--- ATMProject/MainInterface.cs
+++ ATMProject/MainInterface.cs
@@ -14,22 +14,11 @@ namespace ATMProject
     {
         private void MainInterface_Load(object sender, EventArgs e)
         {
-            Account[] ac = new Account[3];
-
-            ATM atm;
-
-            ac[0] = new Account(300, 1111, 111111);
-            ac[1] = new Account(750, 2222, 222222);
-            ac[2] = new Account(3000, 3333, 333333);
-
-            atm = new ATM(ac);
+            
         }
         public MainInterface()
         {
-            InitializeComponent();
-            ButtonFor10p.Hide();
-            buttonfor50.Hide();
-            buttonfor500.Hide();
+    
         }
 
         private void button1_Click(object sender, EventArgs e)
