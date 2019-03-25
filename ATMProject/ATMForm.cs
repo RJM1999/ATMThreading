c52d252faf3b52e0ef9b80bbe7feeef820f2636c
Ross <Ross@DESKTOP-UT87NMM>
2019-03-25 13:23:41 +0000

    Adding classes to files

--- ATMProject/ATMForm.cs
+++ ATMProject/ATMForm.cs
@@ -310,48 +310,6 @@ namespace ATMProject
      *      
      *      the constutor contains the main funcitonality.
      */
-    class ATM
-    {
-        //local referance to the array of accounts
-        private Account[] ac;
-
-        //this is a referance to the account that is being used
-        private Account activeAccount = null;
-
-        // the atm constructor takes an array of account objects as a referance
-        public ATM(Account[] ac)
-        {
-            this.ac = ac;
-        }
-
-        public Account getAccount(int i)
-        {
-            return ac[i];
-        }
-
-        public Account getActiveAccount()
-        {
-            return activeAccount;
-        }
-
-        public int getNumOfAccounts()
-        {
-            //Return the size of the account array
-            return this.ac.GetLength(0);
-        }
-
-        public void setActiveAccount(Account newActiveAccount)
-        {
-            activeAccount = newActiveAccount;
-        }
-
-        public int getActiveAccountPin()
-        {
-            //Retuern the active accounts pin for validation
-            return activeAccount.getPin();
-        }
-
-    }
 }
 
 
--- Account.cs
+++ Account.cs
@@ -1,49 +1,53 @@
 ﻿using System;
-
-class Account
+namespace ATMProject
 {
-    //the attributes for the account
-    private int balance;
-    private int pin;
-    private int accountNum;
+    class Account
+    {
+        //the attributes for the account
+        private int balance;
+        private int pin;
+        private int accountNum;
 
-    // a constructor that takes initial values for each of the attributes (balance, pin, accountNumber)
-    public Account(int balance, int pin, int accountNum)
-    {
-        this.balance = balance;
-        this.pin = pin;
-        this.accountNum = accountNum;
-    }
-    public Boolean decrementBalance(int amount)
-    {
-        if (this.balance >= amount)
+        // a constructor that takes initial values for each of the attributes (balance, pin, accountNumber)
+        public Account(int balance, int pin, int accountNum)
         {
-            balance -= amount;
-            return true;
+            this.balance = balance;
+            this.pin = pin;
+            this.accountNum = accountNum;
         }
-        else
+        public Boolean decrementBalance(int amount)
         {
-            return true;
+            if (this.balance >= amount)
+            {
+                balance -= amount;
+                return true;
+            }
+            else
+            {
+                return true;
+            }
         }
-    }
-    //getter and setter functions for balance
-    public int getBalance()
-    {
-        return balance;
-    }
-    public void setBalance(int newBalance)
-    {
-        this.balance = newBalance;
-    }
+        //getter and setter functions for balance
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void setBalance(int newBalance)
+        {
+            this.balance = newBalance;
+        }
 
-    public int getAccountNum()
-    {
-        return this.accountNum;
-    }
+        public int getAccountNum()
+        {
+            return this.accountNum;
+        }
 
-    public int getPin()
-    {
-        return this.pin;
-    }
+        public int getPin()
+        {
+            return this.pin;
+        }
 
+    }
 }
+
+
